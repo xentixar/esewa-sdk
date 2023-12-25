3149c1d9ee886ce6a49ed2a284c22f9fbc97316c
xentixar <xentixar@gmail.com>
2023-12-25 18:07:43 +0545

    feat:create esewa-sdk

---
+++ README.md
@@ -0,0 +1,83 @@
+# Esewa SDK for PHP
+
+This PHP SDK provides a simple interface to integrate Esewa payment gateway functionality into your application. With this SDK, you can easily configure payment details, generate payment forms, decode responses, and validate transactions using Esewa's APIs.
+
+## Installation
+
+You can install this SDK via Composer. Run the following command in your terminal:
+
+```bash
+composer require xentixar/esewa-sdk
+```
+
+## Usage
+
+### Configuration
+
+- To configure the Esewa payment details, you can use the config method:
+
+```php
+use Xentixar\EsewaSdk\Esewa;
+
+$esewa = new Esewa();
+$esewa->config('https://your-success-url.com', 'https://your-failure-url.com', 1000.00);
+```
+
+- In production mode, to change the secret key given by Esewa, goto src/Esewa.php and replace the `8gBm/:&EnhH.1/q` with the original one in `$this->secret_key = "8gBm/:&EnhH.1/q";`.
+
+### Initialize Payment Form
+
+Development Environment  
+
+- To generate a payment form for the development environment, use the init method without any parameters:
+
+```php
+$esewa->init();
+```
+
+Production Environment  
+
+- To generate a payment form for the production environment, use the init method with the $production parameter set to true:
+
+```php
+$esewa->init(true);
+```
+
+### Decode Response
+
+- After the payment process, you can decode the Esewa response using the decode method:
+
+```php
+$responseData = $esewa->decode();
+```
+
+### Validate Transaction
+
+- You can validate a transaction using the validate method:
+
+```php
+$response = $esewa->validate('1000.00', 'your-transaction-uuid', true);
+```
+
+## Methods:
+
+`config(string $success_url, string $failure_url, float $amount, string $product_code = 'EPAYTEST', float $tax_amount = 0, float $product_service_charge = 0, float $product_delivery_charge = 0)`
+
+- Configures Esewa payment details.
+
+`init(bool $production = false)`
+
+- Initializes the payment form for either the development or production environment based on the `$production` flag.
+
+`decode(): ?array`
+
+- Decodes Esewa response, if the `data` parameter is set in the GET request.
+
+`validate(string $total_amount, string $transaction_uuid, bool $production = false, string $product_code = 'EPAYTEST'): string`
+
+- Validates the transaction by making a cURL request to check the transaction status based on provided parameters.
+
+## Security Note:
+
+- The secret key (`$this->secret_key = "8gBm/:&EnhH.1/q";`) is hardcoded in the class. Ensure that it's stored securely and not exposed in your codebase or version control system.
+- When dealing with transactions and security, always ensure that you validate transaction responses to ensure authenticity and avoid fraudulent transactions.
---
+++ src/Esewa.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Xentixar\EsewaSdk;
+
+class Esewa
+{
+    protected float $amount;
+    protected float $tax_amount;
+    protected float $total_amount;
+    protected string $transaction_uuid;
+    protected string $product_code;
+    protected float $product_service_charge;
+    protected float $product_delivery_charge;
+    protected string $success_url;
+    protected string $failure_url;
+    protected string $signed_field_names;
+    protected string $signature;
+    protected string $secret_key;
+
+    public function config(string $success_url, string $failure_url, float $amount, string $product_code = 'EPAYTEST', float $tax_amount = 0, float $product_service_charge = 0, float $product_delivery_charge = 0)
+    {
+        $this->success_url = $success_url;
+        $this->failure_url = $failure_url;
+        $this->amount = $amount;
+        $this->tax_amount = $tax_amount;
+        $this->product_code = $product_code;
+        $this->product_service_charge = $product_service_charge;
+        $this->product_delivery_charge = $product_delivery_charge;
+        $this->signed_field_names = "total_amount,transaction_uuid,product_code";
+        $this->total_amount = $this->tax_amount + $this->amount + $this->product_delivery_charge + $this->product_service_charge;
+        $this->secret_key = "8gBm/:&EnhH.1/q";
+        $this->transaction_uuid = uniqid() . time();
+        $this->signature = $this->generateHmacSignature($this->total_amount, $this->transaction_uuid, $this->product_code, $this->secret_key);
+    }
+
+    public function init(bool $production = false)
+    {
+        $postData = [
+            "amount" => $this->amount,
+            "failure_url" => $this->failure_url,
+            "product_delivery_charge" => $this->product_delivery_charge,
+            "product_service_charge" => $this->product_service_charge,
+            "product_code" => $this->product_code,
+            "signature" => $this->signature,
+            "signed_field_names" => "total_amount,transaction_uuid,product_code",
+            "success_url" => $this->success_url,
+            "tax_amount" => $this->tax_amount,
+            "total_amount" => $this->total_amount,
+            "transaction_uuid" => $this->transaction_uuid
+        ];
+
+        if ($production) {
+            $url = "https://epay.esewa.com.np/api/epay/main/v2/form";
+        } else {
+            $url = "https://rc-epay.esewa.com.np/api/epay/main/v2/form";
+        }
+
+        echo "<form id='esewaForm' action='$url' method='post'>";
+        foreach ($postData as $key => $value) {
+            echo '<input type="hidden" name="' . $key . '" value="' . $value . '">';
+        }
+        echo '</form>';
+        echo '<script type="text/javascript">document.getElementById("esewaForm").submit();</script>';
+    }
+
+    public function decode()
+    {
+        if (isset($_GET['data'])) {
+            $data = $_GET['data'];
+            $jsonString =  base64_decode($data);
+            $dataArray = json_decode($jsonString, true);
+            return $dataArray;
+        }
+    }
+
+    public function validate(string $total_amount, string $transaction_uuid, bool $production = false, string $product_code = 'EPAYTEST')
+    {
+        if (!$production) {
+            $url = "https://uat.esewa.com.np/api/epay/transaction/status/?product_code=$product_code&total_amount=$total_amount&transaction_uuid=$transaction_uuid";
+        } else {
+            $url = "https://epay.esewa.com.np/api/epay/transaction/status/?product_code=$product_code&total_amount=$total_amount&transaction_uuid=$transaction_uuid";
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if (!$production) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        $response = curl_exec($ch);
+        return $response;
+    }
+
+    protected function generateHmacSignature($total_amount, $transaction_uuid, $product_code, $secret_key): string
+    {
+        $data = "total_amount=" . $total_amount . ",transaction_uuid=" . $transaction_uuid . ",product_code=" . $product_code;
+        $signature = hash_hmac('sha256', $data, $secret_key, true);
+        $base64Signature = base64_encode($signature);
+        return $base64Signature;
+    }
+}
